d25dbe80d0c7b113160153e9dfbf1c0b744a6306
Richard <richardlwhittington@gmail.com>
2026-06-12 22:54:09 -0500

    install daisy ui and name

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,12 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <h1>What will I build?</h1>
-      <h3>Project Must Have</h3>
-      <ol>
-        <li>Something I want to see exist</li>
-        <li>Solves a Real Problem</li>
-        <li>Im excited about it</li>
-        <li>FE and BE</li>
-      </ol>
+    <div>
+      <main>
+      <h1>Squirrel!</h1>
+      <h3>ADHD Progess Tracker</h3>
+      
+   
       </main>
     </div>
   );
