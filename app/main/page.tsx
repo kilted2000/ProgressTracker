ac54a02c7477cb251d4c30f189a48ee268712ef1
Richard <richardlwhittington@gmail.com>
2026-06-14 12:33:44 -0500

    start adding proj cards and center landing page link

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,10 +1,17 @@
-import "../globals.css";
+import Link from "next/link";
 
 export default function mainPage(){
     return(
            <>
-    <h1 className="text-white">Hello</h1>
-<button className="btn btn-active btn-primary">Primary</button>
+    <h1>Projects</h1>
+<Link href="/" className="w-14 border-2 border-rose-950">Home</Link>
+<div className="card w-96 bg-blue-900 card-md border-b-5 border-rose-950">
+  <div className="card-body">
+    <h2 className="card-title">BBRT</h2>
+    <p>Web app for planning baseball roadtrips</p>
+    <progress className="progress progress-success w-56" value="70" max="100"></progress>
+  </div>
+</div>
     </>
     )
  
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
       <main>
       <h1>Squirrel!</h1>
       <h3>ADHD Project Progess Tracker</h3>
-     <Link href="/main">Main Page</Link>
-      
+      <div className= 'justify-self-center pt-75'>
+     <Link href="/main" className='bg-blue-950 border-2 border-rose-950'>Main Page</Link>
+      </div>
    
       </main>
     </div>
