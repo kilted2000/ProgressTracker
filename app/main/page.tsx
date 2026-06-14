b207ca4e8ea3d3d01b1b9c112edfc2af82aa46e7
Richard <richardlwhittington@gmail.com>
2026-06-14 00:34:21 -0500

    add next link and tailwind/daisy styling

---
+++ app/main/page.tsx
@@ -0,0 +1,12 @@
+import "../globals.css";
+
+export default function mainPage(){
+    return(
+           <>
+    <h1 className="text-white">Hello</h1>
+<button className="btn btn-active btn-primary">Primary</button>
+    </>
+    )
+ 
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <main>
       <h1>Squirrel!</h1>
-      <h3>ADHD Progess Tracker</h3>
+      <h3>ADHD Project Progess Tracker</h3>
+     <Link href="/main">Main Page</Link>
       
    
       </main>
